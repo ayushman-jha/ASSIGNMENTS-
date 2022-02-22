54e7877609c8a1ef626639606925cfd2c85d78d3
Ayushman Jha <87076549+ayushman-jha@users.noreply.github.com>
2022-02-22 16:15:13 +0530

    Add files via upload

---
+++ Generics/GenericsQ1.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+public class GenericsQ1 {
+		    
+		        // Creating a HashSet
+	      public static void main(String[] args) {
+		        Set<String> Employee = new HashSet<>();
+		        
+		        {
+		        // Adding new elements to the HashSet
+		        Employee.add("Id-RA1811003030558");
+		        Employee.add("Name-Ayushman jha");
+		        Employee.add("Salary-30000");
+		        Employee.add("Department-Java Full Strack Developer");
+		        System.out.println(Employee);
+		        
+		    }
+		    	}
+		       
+		        	
+
+	}
---
+++ Generics/GenericsQ4.java
@@ -0,0 +1,32 @@
+class Generic1<K,V>{
+	K keyK;
+	V value;
+	public K getKey() {
+		return keyK;
+	}
+	public void setKey(K keyK) {
+		this.keyK = keyK;
+	}
+	public V getValue() {
+		return value;
+	}
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+}
+public class GenericsQ4 {
+	public static void main(String[] args) {
+		Generic1<String, String> obj1 = new Generic1<>();
+		obj1.setKey("1");
+		obj1.setValue("Hello"); 
+		Generic1<String,java.util.Date> obj2 = new Generic1<>();
+		obj2.setKey("Today is");
+		obj2.setValue(new java.util.Date()); 
+		System.out.println(obj1.getKey());
+		System.out.println(obj1.getValue());
+		System.out.println(obj2.getKey()+ obj2.getValue());
+
+	}
+
+}
