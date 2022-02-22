1dfdbfa554e51006b8ca5dc30712e333694a6b47
Ayushman Jha <87076549+ayushman-jha@users.noreply.github.com>
2022-02-22 16:16:28 +0530

    Add files via upload

---
+++ Annotation/AnnotationQ1.java
@@ -0,0 +1,21 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Test{
+}
+class TestMethod{
+	@Test
+     void run() {
+    	 System.out.println("HOW");
+     }
+}
+public class AnnotationQ1 {
+	public static void main(String[] args) {
+		TestMethod s = new TestMethod();
+		s.run();
+	}
+}
