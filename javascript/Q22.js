50fae0bff4508e1c7a7271dda27279e50e8e81ed
Ayushman Jha <87076549+ayushman-jha@users.noreply.github.com>
2022-04-03 14:31:32 +0530

    Add files via upload

---
+++ javascript/Q22.js
@@ -0,0 +1,11 @@
+{
+    var arr = [1, 2, 3, 4, 5];
+    var rotation = parseInt(prompt("Enter the Number oF rotations"));
+    var i = 0;
+    while (i <= rotation - 1) {
+        leftRotatebyOne(arr);
+        i++;
+    }
+    document.write= "The Array after " + rotation + " rotations  is: " + arr;
+
+}
